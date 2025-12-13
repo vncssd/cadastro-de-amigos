6815852ea1f385be5c3d410060de8b00de471a08
Vincius <viniviniee11@gmail.com>
2025-12-13 10:33:03 -0300

    initial commit

---
+++ src/test/java/com/vinicin/amigos/AmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicin.amigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
